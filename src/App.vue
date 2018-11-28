5895801ecac356a871e2488a685d80f6dc047061
home <kulou322@126.com>
2018-11-29 00:47:12 +0800

    vue.js入门https://www.imooc.com/learn/694

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-    <h1 v-text="title"></h1>
+    <h1 v-html="title"></h1>
+    <p>son tell father :{{sonMsg}}</p>
+    <component-a msgFromFather="msg from father" v-on:son-tell-father="sonTellMe"></component-a>
+    <input v-model="newItem" v-on:keyup.enter="addNewItem" />
     <ul>
-      <li v-for="item in items" v-bind:class="{finished: item.finished}" >
+      <li v-for="item in items" v-bind:class="{finished: item.finished}" v-on:click="itemTap(item)">
         {{item.label}}
       </li>
     </ul>
@@ -10,21 +13,41 @@
 </template>
 
 <script>
+import Store from "./store.js";
+import ComponentA from "./components/componentA"
+
 export default {
   data (){
     return {
       title : '<span>LLL</span>This is a todo list.',
-      items : [
-        {
-          label:'vue learning',
-          finished:true,
-        },
-        {
-          label:'swiming',
-          finished:false,
-        },
-      ],
-      thisisli:'ddde'
+      items : Store.fetch(),
+      thisisli:'ddde',
+      newItem:'',
+      sonMsg:'none'
+    }
+  },
+  components:{ComponentA},
+  methods :{
+    addNewItem:function () {
+      this.items.push({
+        label:this.newItem,
+        finished:false
+      }),
+      this.newItem = ''
+    },
+    itemTap:function (item) {
+      item.finished = !item.finished
+    },
+    sonTellMe:function (sonMsg) {
+     this.sonMsg = sonMsg
+    }
+  },
+  watch:{
+    items : {
+      handler (items) {
+        Store.save(items)
+      },
+      deep:true
     }
   }
 }
---
+++ src/components/componentA.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <h2>组件componentA</h2>
+    <p v-text="msgFromFather" v-on:click="tapAction"></p>
+ </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            msg:'son',
+            msgFromSon:'msg from son'
+        }
+    },
+    props:[
+        'msgFromFather'
+    ],
+    methods : {
+        tapAction () {
+            this.$emit("son-tell-father",this.msgFromSon)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
