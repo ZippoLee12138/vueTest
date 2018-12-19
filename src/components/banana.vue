de7b6865cdaab35944d1d230f6a913b86f73680a
home <kulou322@126.com>
2018-12-19 23:15:15 +0800

    合并冲突

---
+++ src/components/banana.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        banana component
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,26 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import Router from 'vue-router'
+import Apple from "./components/apple"
+import Banana from "./components/banana"
 
 Vue.config.productionTip = false
+
+Vue.use(Router)
+
+let router = new Router({
+  routes: [
+    {
+      path: '/apple',
+      component: Apple
+    },
+    {
+      path: '/banana',
+      component: Banana
+    }
+  ]
+})
 
 /* eslint-disable no-new */
 new Vue({
