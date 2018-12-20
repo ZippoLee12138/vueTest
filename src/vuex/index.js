867c64fbf9e65c8042cbff9bae58cf76722a5e39
lizebo <lizebo@koopoo.com.cn>
2018-12-20 20:22:10 +0800

    开启ESLint

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -47,6 +47,6 @@ export default new Vuex.Store({
     }
   },
   modules: {
-    KFCList,
+    KFCList
   }
 })
--- src/vuex/moduleA.js
+++ src/vuex/moduleA.js
@@ -1,31 +1,31 @@
 export default {
   state: {
-      productions: [
-          {
-              name: '汉堡',
-              price: 20
-          }, {
-              name: '上校鸡腿',
-              price: 25
-          }, {
-              name: '可口可乐',
-              price: 5
-          }
-      ]
+    productions: [
+      {
+        name: '汉堡',
+        price: 20
+      }, {
+        name: '上校鸡腿',
+        price: 25
+      }, {
+        name: '可口可乐',
+        price: 5
+      }
+    ]
   },
   mutations: {
-      saleAction (state, payload) {
-          state.productions.map(product => {
-                product.price /= 2
-          })
-      }
+    saleAction (state, payload) {
+      state.productions.map(product => {
+        product.price /= 2
+      })
+    }
   },
   actions: {
-      saleAction (content, payload) {
-        setTimeout(() => {
-            content.commit('saleAction', payload)
-        }, 1000);
-      }
+    saleAction (content, payload) {
+      setTimeout(() => {
+        content.commit('saleAction', payload)
+      }, 1000)
+    }
   },
   getters: {
 
